101137ffbdaa0d9c89079fe0ddd0ecc729aef956
rishikesh18 <rishi.neupane@gmail.com>
2018-07-18 12:22:38 -0400

    checking change

---
+++ src/components/newsScraper.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewsScrapper extends Component {
+  render() {
+    return (
+      <div className="App">
+        Rishikesh Project
+      </div>
+    );
+  }
+}
+
+export default NewsScrapper;
